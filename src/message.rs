be6e967b8530af68c56e94b304965d62b2db88a2
Jasper Meggitt <jasper.meggitt@gmail.com>
2022-09-26 09:49:56 -0400

    Refactor previous testing

---
+++ src/message.rs
@@ -0,0 +1,153 @@
+use byteorder::{BigEndian, ByteOrder};
+use bytes::BytesMut;
+use std::io;
+use std::io::ErrorKind::{InvalidData, InvalidInput, UnexpectedEof};
+use std::io::{Error, Read};
+
+pub struct BGPChunks<R> {
+    next_chunk: BytesMut,
+    chunk_size: usize,
+    reader: R,
+    finished: bool,
+}
+
+impl<R> BGPChunks<R> {
+    pub fn new(reader: R) -> Self {
+        Self::with_capacity(32 * 1024, reader)
+    }
+
+    pub fn with_capacity(capacity: usize, reader: R) -> Self {
+        BGPChunks {
+            chunk_size: capacity,
+            next_chunk: BytesMut::new(),
+            reader,
+            finished: false,
+        }
+    }
+}
+
+impl<R: Read> BGPChunks<R> {
+    fn read_at_least(&mut self, length: usize) -> io::Result<usize> {
+        let previous_length = self.next_chunk.len();
+        let target_length = length.max(self.chunk_size);
+        self.next_chunk.resize(target_length, 0);
+
+        let mut fill = previous_length;
+        while fill < target_length {
+            match self.reader.read(&mut self.next_chunk[fill..])? {
+                0 => break,
+                x => fill += x,
+            }
+        }
+
+        self.next_chunk.truncate(fill);
+
+        if fill < length {
+            // Unable to fill buffer to required length
+            return Err(Error::from(UnexpectedEof));
+        }
+
+        Ok(fill - previous_length)
+    }
+}
+
+impl<R: Read> Iterator for BGPChunks<R> {
+    type Item = io::Result<BytesMut>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.finished {
+            return None;
+        }
+
+        let header = match MRTCommonHeader::try_from(&*self.next_chunk) {
+            Ok(header) => header,
+            Err(_) => {
+                match self.read_at_least(MRTCommonHeader::LENGTH) {
+                    Ok(_) => {}
+                    Err(e) if e.kind() == UnexpectedEof => {
+                        self.finished = true;
+                        return None;
+                    }
+                    Err(e) => {
+                        self.finished = true;
+                        return Some(Err(e));
+                    }
+                }
+
+                MRTCommonHeader::try_from(&*self.next_chunk)
+                    .expect("should be infallible due to previous read_at_least")
+            }
+        };
+
+        let length_required = MRTCommonHeader::LENGTH + header.length() as usize;
+        if self.next_chunk.len() < length_required {
+            if let Err(e) = self.read_at_least(length_required) {
+                self.finished = true;
+                return Some(Err(e));
+            }
+        }
+
+        Some(Ok(self.next_chunk.split_to(length_required)))
+    }
+}
+
+pub struct MRTCommonHeader<'a> {
+    buffer: &'a [u8; 12],
+}
+
+impl<'a> MRTCommonHeader<'a> {
+    /// The header occupies 12 bytes at the start of each message
+    const LENGTH: usize = 12;
+
+    pub fn timestamp(&self) -> u32 {
+        BigEndian::read_u32(&self.buffer[0..4])
+    }
+
+    pub fn msg_type(&self) -> io::Result<MrtType> {
+        match BigEndian::read_u16(&self.buffer[4..6]) {
+            11 => Ok(MrtType::OSPFv2),
+            12 => Ok(MrtType::TABLE_DUMP),
+            13 => Ok(MrtType::TABLE_DUMP_V2),
+            16 => Ok(MrtType::BGP4MP),
+            17 => Ok(MrtType::BGP4MP_ET),
+            32 => Ok(MrtType::ISIS),
+            33 => Ok(MrtType::ISIS_ET),
+            48 => Ok(MrtType::OSPFv3),
+            49 => Ok(MrtType::OSPFv3_ET),
+            _ => Err(Error::from(InvalidData)),
+        }
+    }
+    pub fn sub_type(&self) -> u16 {
+        BigEndian::read_u16(&self.buffer[6..8])
+    }
+    pub fn length(&self) -> u32 {
+        BigEndian::read_u32(&self.buffer[8..12])
+    }
+}
+
+impl<'a> TryFrom<&'a [u8]> for MRTCommonHeader<'a> {
+    type Error = io::Error;
+
+    #[inline(always)]
+    fn try_from(value: &'a [u8]) -> Result<Self, Self::Error> {
+        // Enforce that the length is checked at most once.
+        match value.get(..Self::LENGTH).map(|x| x.try_into()) {
+            Some(Ok(buffer)) => Ok(MRTCommonHeader { buffer }),
+            _ => Err(Error::from(InvalidInput)),
+        }
+    }
+}
+
+#[derive(Copy, Clone, Hash, Ord, PartialOrd, Eq, PartialEq, Debug)]
+#[repr(u16)]
+pub enum MrtType {
+    OSPFv2 = 11,
+    TABLE_DUMP = 12,
+    TABLE_DUMP_V2 = 13,
+    BGP4MP = 16,
+    BGP4MP_ET = 17,
+    ISIS = 32,
+    ISIS_ET = 33,
+    OSPFv3 = 48,
+    OSPFv3_ET = 49,
+}
