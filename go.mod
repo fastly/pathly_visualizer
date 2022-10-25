7a92a1d8dcd77edd7c80411abe5bbf7fbde73e65
Jasper Meggitt <jasper.meggitt@gmail.com>
2022-10-25 12:18:13 -0400

    Add basic go module to begin working off of

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/jmeggitt/fastly_anycast_experiments.git
+
+go 1.19
+
+require (
+	github.com/joho/godotenv v1.4.0
+)
---
+++ server.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"github.com/joho/godotenv"
+	"log"
+)
+
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Printf("Error loading .env file: %s\n", err.Error())
+		log.Println("Configuration will be loaded from environment variables instead")
+	}
+
+	// Anything else that should be set up before main
+}
+
+func main() {
+	log.Println("hello world")
+}
