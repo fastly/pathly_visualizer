8c923759c6edd8a8ea5a6bf50062e878af539dd9
Jasper Meggitt <jasper.meggitt@gmail.com>
2022-11-07 11:17:35 -0500

    Experiment with using a service model

--- src/common/state.go
+++ src/common/state.go
@@ -8,7 +8,25 @@ package common
 // other services will do a mix of reading and writing. There should be one concurrency structures in here for each
 // piece of the state that can be used in isolation.
 type ApplicationState struct {
+	// TODO: Add state data
 }
 
+// InitApplicationState created the initial state to use upon the start of the application. This function is
+// responsible for doing the initial setup for any data that is not managed by a service
+func InitApplicationState() *ApplicationState {
+	// Zero initialize for now
+	return new(ApplicationState)
+}
 
+// TODO: Is the service model the one I want to use?
+type Service interface {
+	// Name provides the name of the service. This is only used for logging.
+	Name() string
 
+	// Init sets up the state of this service
+	Init(state *ApplicationState) error
+
+	// Run begins execution of the service. This is assumed to consume the entire thread and should only exit upon
+	// encountering a fatal error.
+	Run(state *ApplicationState) error
+}
