feee3902037f14dacbc46b7b8307043f793f078c
YashPatel5 <yppatel@wpi.edu>
2022-11-10 11:12:48 -0500

    Added Ipv6 file tests

--- tests/test-traceroute-data/traceroute-ingestion_test.go
+++ tests/test-traceroute-data/traceroute-ingestion_test.go
@@ -179,6 +179,39 @@ func TestFileTracerouteData(t *testing.T) {
 	}
 }
 
+func TestIPv6FileTracerouteData(t *testing.T) {
+	actualTraceroute, err := getTracerouteMeasurement("small_BRootIPv6Traceroute.json")
+	if err != nil {
+		t.Errorf("Could not read from small_BRootIPv6Traceroute.json. Error: %+v", err)
+	}
+
+	expectedSrcAddr := []string{"2a01:4f8:1c17:6262::1", "2a10:3781:2393:1:220:4aff:fec8:2099", "2a04:6480:204:0:1:44ff:fe1d:53b2", "2a02:a465:8ead:1:1:dff:fe08:5119"}
+	expectedDstAddr := "2001:500:200::b"
+	expectedHopCounts := []int{18, 15, 12, 13}
+
+	//Check for Src Address
+	for i, traceroute := range actualTraceroute {
+		if traceroute.SrcAddr() != expectedSrcAddr[i] {
+			t.Errorf("Incorrect Src Addr for BRootIPv6. Expected %v, but got %v\n", expectedSrcAddr[i], traceroute.SrcAddr())
+		}
+	}
+
+	//Check for Dst Addr
+	for _, traceroute := range actualTraceroute {
+		if traceroute.DstAddr() != expectedDstAddr {
+			t.Errorf("Incorrect Dst Addr for BRootIPv6. Expected %v, but got %v\n", expectedDstAddr, traceroute.SrcAddr())
+		}
+	}
+
+	//Check Hop counts
+	for i, traceroute := range actualTraceroute {
+		if len(traceroute.TracerouteResults()) != expectedHopCounts[i] {
+			t.Errorf("Incorrect Number of hops for BRootIPv6. Expected %v, but got %v\n", len(traceroute.TracerouteResults()), expectedHopCounts[i])
+		}
+	}
+
+}
+
 func getTracerouteMeasurement(fileName string) ([]measurement.Result, error) {
 	// Read Atlas results from a file
 	a := ripeatlas.Atlaser(ripeatlas.NewFile())
