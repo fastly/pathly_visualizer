a9dfe8c431ca27c472c8334a72f91c668d91b84a
jcampanale <jcampanale@wpi.edu>
2022-12-05 13:35:10 -0500

    update app.test.js

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+// not sure what other tests to include
+
+test('renders app', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
 });
