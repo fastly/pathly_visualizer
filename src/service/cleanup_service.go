e2b87bd0ab7e61a50e16cbf2371b4e571e1da8bf
YashPatel50 <patelshay8@gmail.com>
2022-12-06 10:39:31 -0500

    Added base service

---
+++ src/service/cleanup_service.go
@@ -0,0 +1,16 @@
+package service
+
+type CleanupService struct{}
+
+func (CleanupService) Name() string {
+	return "CleanupService"
+}
+
+func (CleanupService) Init(state *ApplicationState) (err error) {
+
+	return
+}
+
+func (service CleanupService) Run(state *ApplicationState) (err error) {
+	return
+}
