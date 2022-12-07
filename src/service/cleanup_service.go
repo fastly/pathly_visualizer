55d1e33f213a0e7247e682ad550916656f04f324
YashPatel50 <patelshay8@gmail.com>
2022-12-07 11:14:37 -0500

    Clean up traceroute path data

--- src/service/cleanup_service.go
+++ src/service/cleanup_service.go
@@ -30,11 +30,17 @@ func (service CleanupService) Run(state *ApplicationState) (err error) {
 
 		//Wait for cleanup until Statistic Period has passed
 		if timeElapsed < service.CleanupPeriod {
-
+			//TODO: Something to force it to wait
 		} else {
 			//Cleanup once the statistics period has reached
-
+			state.TracerouteDataLock.Lock()
+			state.TracerouteData.EvictOutdatedData()
+			service.LastCleanup = time.Now()
+			state.TracerouteDataLock.Unlock()
 		}
 	}
-	return
 }
+
+//I think the traceroute data is the main thing, but generally any data that was last used more than the statistics period ago.
+//That includes removing all old nodes/edges from routes and removing any routes that no-longer have any up to date data.
+//I am thinking we probably want to clear it about once a day?
